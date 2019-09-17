5057cf2200f51a11d7b9c469cb26b49ab69e5415
3Dblog maker <narutoshipfan@gmail.com>
2019-09-17 10:20:55 +0300

    Delete storages2.js

--- storages2.js
+++
@@ -1,6 +0,0 @@
-if(localStorage.getItem('Wcolor')){
-	document.getElementById("on1").style.backgroundColor = localStorage.getItem('Wcolor');
-	}
-if(localStorage.getItem('Tcolor')){
-	document.getElementById("on2").style.backgroundColor = localStorage.getItem('Tcolor');
-	}
