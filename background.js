6eee5072f06fae8c16e29ec62a8c57edd5811569
3Dblog maker <narutoshipfan@gmail.com>
2019-09-17 10:13:48 +0300

    Add files via upload

---
+++ background.js
@@ -0,0 +1,8 @@
+  
+  var surf="SURF";
+  //if(){}else{}
+  //chrome.browserAction.setBadgeText({text: 'SURF'});
+  chrome.browserAction.setBadgeText({text: ''+surf+''});
+  chrome.browserAction.setBadgeBackgroundColor({color: '#4688F1'});
+  
+ // *://*.blogspot.com/*
---
+++ storages2.js
@@ -0,0 +1,6 @@
+if(localStorage.getItem('Wcolor')){
+	document.getElementById("on1").style.backgroundColor = localStorage.getItem('Wcolor');
+	}
+if(localStorage.getItem('Tcolor')){
+	document.getElementById("on2").style.backgroundColor = localStorage.getItem('Tcolor');
+	}
